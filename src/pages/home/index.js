9d343843a0e230e2ffc43356c8461629f9a5f0ed
izaleu <steel.princess@gmail.com>
2018-07-28 12:59:24 -0700

    Homepage CSS

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './index.css'
 
 class Home extends Component {
     render() {
         return (
-            <div className="container">
+            <div className={styles.homeContainer}>
                 <p>Welcome to <Link className="nav-link" to="/">code-witch.net!</Link> <br/> </p>
                 <p>This is where my various projects live.</p>
             </div>
