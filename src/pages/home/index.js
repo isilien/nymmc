f615617bd5c242ae06744655d80770c64bdd32a2
izaleu <steel.princess@gmail.com>
2018-07-27 17:32:41 -0700

    Add homepage intro

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                Nothing here yet.
+            <div className="container">
+                Welcome to <Link className="nav-link" to="/">code-witch.net!</Link> <br/> 
+                This is where I put all of my random, miscellanous projects.
             </div>
         );
     }
