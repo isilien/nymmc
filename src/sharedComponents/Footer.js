dc7663d8e802efb300b805b9cea3d383232788d4
izaleu <steel.princess@gmail.com>
2018-07-27 18:00:59 -0700

    Add footer but don't display it.

---
+++ src/sharedComponents/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render () {
+        return (
+            <div>
+            <small>Proudly <i>not</i> made with create-react-app because even I'm not *that* lazy.</small>
+            </div>
+        )
+    }
+}
+
+export default Footer;
--- src/sharedComponents/index.js
+++ src/sharedComponents/index.js
@@ -1,5 +1,8 @@
 import Header from './Header';
+import Footer from './Footer';
 import Placeholder from './Placeholder';
 
+
 export {Header as Header}
+export {Footer as Footer}
 export {Placeholder as Placeholder}
