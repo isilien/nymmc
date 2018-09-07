02212d8f6e01b44ae98375a94d4e09c56dd72f18
izaleu <steel.princess@gmail.com>
2018-09-07 15:56:29 -0700

    Tutorial stub out

--- src/pages/mission/index.js
+++ src/pages/mission/index.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import { Link } from 'react-router-dom';
 import './index.css'
 
 class Mission extends Component {
     render() {
+
+        const {id} = this.props.match.params;
         return (
-            <div>
-                Mission Start
+            <div className="container">
+                <div className="subheader">
+                    <h2>Mission {id} <Link className="btn btn-success" to="/"><i className="fas fa-arrow-left"/> Back</Link></h2>
+                    </div>
+                <div className="missionContent">
+
+                </div>
             </div>
         );
     }
 }
+
 export default Mission;
--- src/pages/tutorial/index.js
+++ src/pages/tutorial/index.js
@@ -5,8 +5,17 @@ import { Link } from 'react-router-dom';
 class Tutorial extends Component {
     render() {
         return (
-            <div>
-                Tutorial Start
+            <div className="container">
+                <span><h2>Tutorial <Link className="btn btn-success" to="/"><i class="fas fa-arrow-left"/> Back</Link></h2></span>
+                <ul>
+                    <li>Press Start</li>
+                    <li>timer counts down</li>
+                    <li>drag resources from your hand into the correct slot</li>
+                    <li>once all slots are filled, a new challenge will appear</li>
+                    <li>Tip: use agile button if you get stuck</li>
+                    <li>some cards can be actvated, either to draw more cards, defeat a challenge, etc</li>
+                    <li>If time runs out before launch, you lose</li>
+                </ul>
             </div>
         );
     }
