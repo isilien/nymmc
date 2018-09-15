4e220bc5ba9fdfc0a0066c6f753712b3caf2db4a
izaleu <steel.princess@gmail.com>
2018-09-15 14:19:43 -0700

    Draggable

--- src/pages/mission/index.js
+++ src/pages/mission/index.js
@@ -1,16 +1,26 @@
+import _ from 'underscore'
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment'
 import { Link } from 'react-router-dom';
 import Countdown from 'react-countdown-now';
-import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
+import { Container, Draggable } from 'react-smooth-dnd';
 
 
 import MissionCountdown from './countdown';
 import actionCreators from '../../modules/mission/actions'
 import './index.css'
 import ChallengeCard from './challengeCard'
-import data from '../../assets/exampleChallengeCardData.json'
+import exampleData from '../../assets/exampleChallengeCardData.json'
+import resourceCardDefs from '../../assets/resourceCardDefs.json'
+import SimpleSortableList from './SimpleSortableList';
+import './card.css'
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  }
 
 //TODO: make timer own component
 class Mission extends Component {
@@ -18,8 +28,27 @@ class Mission extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            hasStarted: true, isPaused: true, endTime: moment().add('1', 'm')//endTime: moment(0,'ms')
+            hasStarted: false, 
+            isPaused: true, 
+            endTime: moment().add('1', 'm'),//endTime: moment(0,'ms')
+            
+            hand: [], //array of resource and event cards
+            drawDeck: [], //stack of cards
+            resourcesPile: [], //arr of strings
+            discardPile: [], //stack of cards
+            challengeDeck: exampleData,
         }
+
+        this.state.drawDeck = this.initialDeck(50);
+        this.drawCards(5, this.state.drawDeck, this.state.hand);
+    }
+
+    initialDeck = (size) => {
+        let deck = [];
+        for(let i = 0; i < size; i++) {
+            deck.push(resourceCardDefs[getRandomIntInclusive(0,2)]);
+        }
+        return deck;
     }
 
     startCountdown = () => {
@@ -39,11 +68,57 @@ class Mission extends Component {
         //TODO: show game over modal
         console.log("You lose");
     }
+    
+    //from hand
+    playResource = (card, handIndex) => {
+        let resourceArray = this.state.resourcesPile;
+        resourceArray.push(card.resource);
+
+        let newHand = this.state.hand;
+        newHand.splice(handIndex,1)
+
+        this.setState({resourcesPile: resourceArray, hand: newHand})
+    }
+
+    //side FX
+    drawCards = (amount, source, destination) => {
+        for(let i = 0; i < amount; i++) {
+            destination.push(source.pop());
+        }
+    }
+
+    getRemainingRequirements = (requirements, current) => {
+        const fakeReq = ['ops','ops','dev','dev','UX']
+        const counts = _.countBy(fakeReq);
+        const counts2 = _.countBy(current);
+
+        
+        const result = _.map(Object.keys(counts), cat=> {
+            return counts[cat] - (counts2[cat] || 0)
+        })
+
+        console.log(result)
+
+        return fakeReq;
+    }
 
     render() {
         const { id } = this.props.match.params;
-        const { endTime, startTime, hasStarted, isPaused } = this.state;
+        const { 
+            hasStarted,
+            isPaused,
+            endTime,
+            hand,
+            drawDeck,
+            resourcesPile,
+            discardPile,
+            challengeDeck,
+        } = this.state;
 
+        const currentChallenge = challengeDeck[0];
+        const challengeRequirements = this.getRemainingRequirements(currentChallenge.requirements, resourcesPile);
+
+        console.log(challengeRequirements, hand, resourcesPile)
         return (
             <div className="container">
                 <div className="subheader">
@@ -60,12 +135,36 @@ class Mission extends Component {
                                 <button className="btn btn-danger" onClick={()=>{alert("Are you sure you want to quit?")}}><i className="fas fa-times" /> Quit</button>
                             </div>}
                     </div>
+                    <div className="currentChallenge">
+                        <ChallengeCard {...currentChallenge}/>
+                    </div>
                     <div className="playArea">
-                        <div className="row challenges">
-                            <Draggable><ChallengeCard {...data[0]}/></Draggable>
+                        <div className="row requirements">
+                            <div className="col-12">Requirements</div>
+                            <Container
+                                className="col-12"
+                                orientation="horizontal"
+                                shouldAcceptDrop={(incoming, payload)=>incoming.groupName==="hand" && _.contains(challengeRequirements, payload.resource)}
+                            >
+                                {_.map(challengeRequirements, (requirement, index) => {
+                                    return <Draggable className="resourceCard" key={index}> {requirement} </Draggable>
+                                })}
+                            </Container>
                         </div>
-                        <div className="row requirements"></div>
-                        <div className="row hand"></div>
+                        <div className="row hand">
+                            <div className="col-12">Hand</div>
+                            <Container
+                                className="col-12"
+                                orientation="horizontal"
+                                groupName="hand"
+                                getChildPayload={foo=>hand[foo]}
+                                onDrop={({payload, removedIndex})=>this.playResource(payload, removedIndex)}
+                            >
+                                {_.map(hand, (card, index) => {
+                                    return <Draggable className="card" key={index}> {card.title} </Draggable>
+                                })}
+                            </Container>
+                        </div>
                     </div>
                 </div>
             </div>
