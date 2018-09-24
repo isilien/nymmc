bd63de71370f93c48517fa233d2fbf818e05a96a
izaleu <steel.princess@gmail.com>
2018-09-23 20:00:27 -0700

    Revert back to 5MMC instead of 1MMC

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,7 +7,9 @@ class Home extends Component {
         return (
             <div className="homeContainer">
                 <div>
-                    <p>Welcome to 5-Minute Mission Control: an interactive exercise for <i>Not Your Mom's Mission Control</i>. 5MMC is about learning to spend your resources in an agile, scalable way while you work against the clock. Along the way you'll encounter some of the same challenges that we faced in creating the real Mission Control. Good luck and have fun!</p>
+                    <p>Welcome to 5-Minute Mission Control: an interactive exercise for <i>Not Your Mom's Mission Control</i>. 5MMC is about learning to spend your resources in an agile, scalable way while you work against the clock. Along the way you'll encounter some of the same challenges that we faced in creating the real Mission Control. 
+                    <br/>
+                    <br/> Good luck and have fun! <i> — The Not Your Mom's Mission Control Team</i></p>
                 </div>
                 <div className="col-6 mx-auto">
                     {/* <Link className="btn btn-primary btn-block" to="/tutorial">Tutorial</Link> */}
